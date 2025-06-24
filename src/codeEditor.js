7015e1b8e9c225c3bdeece23855644bd60906851
iblameharsh <thecoachdefence112@gmail.com>
2025-06-24 14:00:21 +0530

    monaco

--- src/codeEditor.js
+++ src/codeEditor.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { io } from 'socket.io-client';
+import Editor from '@monaco-editor/react';
 
 const socket = io('http://localhost:5000');
 
 const CodeEditor = () => {
   const { id: roomId } = useParams();
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState('// Start typing JavaScript code...');
 
   useEffect(() => {
     socket.emit('join', roomId);
 
     socket.on('code-change', (newCode) => {
-      setCode(newCode);
+      if (newCode !== code) {
+        setCode(newCode);
+      }
     });
 
     return () => {
@@ -20,21 +23,26 @@ const CodeEditor = () => {
     };
   }, [roomId]);
 
-  const handleChange = (e) => {
-    const newCode = e.target.value;
-    setCode(newCode);
-    socket.emit('code-change', { roomId, code: newCode });
+  const handleEditorChange = (value) => {
+    setCode(value);
+    socket.emit('code-change', { roomId, code: value });
   };
 
   return (
     <div className="container mt-4">
       <h5>Session ID: <code>{roomId}</code></h5>
-      <textarea
-        className="form-control mt-3"
+
+      <Editor
+        height="80vh"
+        defaultLanguage="javascript"
+        theme="vs-dark"
         value={code}
-        onChange={handleChange}
-        rows={20}
-        placeholder="Start writing your code here..."
+        onChange={handleEditorChange}
+        options={{
+          fontSize: 16,
+          minimap: { enabled: false },
+          wordWrap: "on",
+        }}
       />
     </div>
   );
