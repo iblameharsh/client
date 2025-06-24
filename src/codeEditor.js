ac87e413b47dfc6f4811cea0b8657cd75111dbf5
iblameharsh <thecoachdefence112@gmail.com>
2025-06-24 15:54:10 +0530

    codeEditor

--- src/codeEditor.js
+++ src/codeEditor.js
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 import Editor from '@monaco-editor/react';
 import './CodeEditor.css';
 
-const socket = io('http://localhost:5000');
+const socket = io(process.env.REACT_APP_BACKEND_URL);
 
 const languageOptions = {
   javascript: '// Start typing JavaScript code...',
